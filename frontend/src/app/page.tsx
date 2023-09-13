f92f3f7ae99d8905ba8aec1476ad42b42b67a050
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2023-09-13 11:13:08 -0300

    feat:card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import Card from "@/components/Card";
 import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <div className="grid grid-cols-1 gap-4 p-6 md:grid-cols-4 w-full h-screen place-items-center ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
